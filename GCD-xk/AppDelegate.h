3bfca0d3f8e6b22c0a491a20cb5165a44021fe6f
wangxkK <593976841@qq.com>
2017-07-04 16:12:13 +0800

    Initial Commit

---
+++ GCD-xk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD-xk
+//
+//  Created by 王晓康 on 2017/7/4.
+//  Copyright © 2017年 wxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD-xk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD-xk
+//
+//  Created by 王晓康 on 2017/7/4.
+//  Copyright © 2017年 wxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
